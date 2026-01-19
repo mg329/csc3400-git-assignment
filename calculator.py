ec56177668200790966bfbcb43e9e8dcb782ece1
Matthew Greenblatt <mgreenblatt@mocs.flsouthern.edu>
2026-01-19 15:52:54 -0500

    Update for calculator module

--- calculator.py
+++ calculator.py
@@ -1,37 +1,19 @@
+import math
 def calculator():
-    statement = True
-    while statement:
-        num1 = float(input())
-        operator = input()
-        num2 = float(input())
 
-        result = calculate(num1, num2, operator)
+def add(a, b):
+    return a + b
 
-        if result == -1:
-            print("Cannot divide by zero")
-        elif result == -2:
-            print("Invalid operator")
-        else:
-            print("Result: ", result)
-        
-        question = input("Do another calculation? (yes/no): ").lower()
+def subtract(a, b):
+    return a - b
 
-        if question == "no" or question == "n":
-            statement = False
+def multiply(a, b):
+    return a * b
 
-def calculate(num1, num2, operator):
-    if operator == "+":
-        return num1 + num2
-    elif operator == "-":
-        return num1 - num2
-    elif operator == "*":
-        return num1 * num2
-    elif operator == "/":
-        if num2 == 0:
-            return -1
-        return num1 / num2
-    else:
-        return -2
-    
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a/b
+
 if __name__ == "__main__":
     calculator()
