162d102bf3a6ba4a5823f708ea2bf9fc0bd18484
Matthew Greenblatt <mgreenblatt@mocs.flsouthern.edu>
2026-01-19 15:45:28 -0500

    Fixed advanced mathematical operations

--- calculator.py
+++ calculator.py
@@ -1,50 +1,26 @@
 import math
 def calculator():
-    statement = True
-    while statement:
-        num1 = float(input())
-        operator = input()
-        if operator == "square root":
-            result = squareRoot(num1)
-        else:
-            num2 = float(input())
-            result = calculate(num1, num2, operator)
 
-        if result == -1:
-            print("Cannot divide by zero")
-        elif result == -2:
-            print("Invalid operator")
-        elif result == -3:
-            print("Cannot take square root of negative value")
-        else:
-            print("Result: ", result)
-        
-        question = input("Do another calculation? (yes/no): ").lower()
+def add(a, b):
+    return a + b
 
-        if question == "no" or question == "n":
-            statement = False
+def subtract(a, b):
+    return a - b
 
-def calculate(num1, num2, operator):
-    if operator == "+":
-        return num1 + num2
-    elif operator == "-":
-        return num1 - num2
-    elif operator == "*":
-        return num1 * num2
-    elif operator == "/":
-        if num2 == 0:
-            return -1
-        return num1 / num2
-    elif operator == "^":
-        return num1 ** num2
-    else:
-        return -2
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a/b
     
-def squareRoot(num1):
-    if num1 < 0:
-        return -3
-    return math.sqrt(num1)
-    
+def power(a, b):
+    return a ** b
+
+def square_root(a):
+    if a < 0:
+        raise ValueError("Cannot take square root of a negative number")
 
 if __name__ == "__main__":
     calculator()
