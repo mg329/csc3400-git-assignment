f9e14ca0a4c3e94545b13a6af4a92d031a86bea1
Matthew Greenblatt <mgreenblatt@mocs.flsouthern.edu>
2026-01-19 14:28:02 -0500

    Add advanced mathematical operations

--- calculator.py
+++ calculator.py
@@ -1,16 +1,21 @@
+import math
 def calculator():
     statement = True
     while statement:
         num1 = float(input())
         operator = input()
-        num2 = float(input())
-
-        result = calculate(num1, num2, operator)
+        if operator == "square root":
+            result = squareRoot(num1)
+        else:
+            num2 = float(input())
+            result = calculate(num1, num2, operator)
 
         if result == -1:
             print("Cannot divide by zero")
         elif result == -2:
             print("Invalid operator")
+        elif result == -3:
+            print("Cannot take square root of negative value")
         else:
             print("Result: ", result)
         
@@ -30,8 +35,16 @@ def calculate(num1, num2, operator):
         if num2 == 0:
             return -1
         return num1 / num2
+    elif operator == "^":
+        return num1 ** num2
     else:
         return -2
     
+def squareRoot(num1):
+    if num1 < 0:
+        return -3
+    return math.sqrt(num1)
+    
+
 if __name__ == "__main__":
     calculator()
